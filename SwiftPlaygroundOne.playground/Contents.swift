37130d455040b65984a77d669da0c29777cf243d
Keola Iseri <keolaiseri@gmail.com>
2020-01-21 10:40:14 -1000

    Completed assignment

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -13,3 +13,17 @@ var myThirdVariable = myFirstVariable + mySecondVariable
 
 print(myThirdVariable)
 
+//Part 8 starts here:
+
+//var keolaFtw: String
+
+var keolaFtw = "I love food"
+
+//Declare and assign variable in single line of code
+var keolaFtwsecond = " Food is the bomb"
+
+var keolaFtwThird = " I'm hungry!"
+
+var keolaFtwFourth = keolaFtw + keolaFtwsecond + keolaFtwThird
+
+print(keolaFtwFourth)
